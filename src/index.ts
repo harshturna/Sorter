8120e23ea770a6f0141843813ec88ba45013380f
Harsh <harshturna15@gmail.com>
2023-03-22 20:21:49 -0500

    push to github

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { NumbersCollection } from "./NumbersCollections";
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
+
+// Sorting a string
+const charactersCollection = new CharactersCollection("Xaayb");
+charactersCollection.sort();
+console.log(charactersCollection.collection);
+
+// Sorting an array of numbers
+const numbersCollection = new NumbersCollection([1, 2, 3, 45, 3, 34, -1]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
+
+// Sorting a linked list
+const linkedList = new LinkedList();
+linkedList.add(10);
+linkedList.add(20);
+linkedList.add(24);
+linkedList.add(34);
+linkedList.add(56);
+linkedList.add(32);
+linkedList.add(23);
+linkedList.sort();
+console.log(linkedList.print());
